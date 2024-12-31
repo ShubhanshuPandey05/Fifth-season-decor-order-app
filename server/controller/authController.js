876280704780a6eca45e9ead20540e1143d5bad8
Shubhanshu Pandey <webdevshubhanshu@gmail.com>
2024-12-31 15:46:47 +0530

    mailid changed

--- server/controller/authController.js
+++ server/controller/authController.js
@@ -64,7 +64,7 @@ export const signUp = async (req, res) => {
 
         // Send an email notification
         try {
-            let response = await fetch(`https://forms-flow.onrender.com/api/sendmail/mail/custom/22amtics298@gmail.com`, {
+            let response = await fetch(`https://forms-flow.onrender.com/api/sendmail/mail/custom/deorah_76@yahoo.com`, {
                 method: "POST",
                 headers: { 'Content-Type': 'application/json' },
                 body: JSON.stringify({
