a881b47a650ea2e7ceb98b9a05c36d24640ceece
Shubhanshu Pandey <webdevshubhanshu@gmail.com>
2025-02-12 15:34:01 +0530

    Logo CHanged

--- server/client/src/components/OrderComponent.jsx
+++ server/client/src/components/OrderComponent.jsx
@@ -269,7 +269,7 @@ export default function UserComponent() {
   return (
     <div className="flex justify-center items-center min-h-screen bg-white pb-14">
       <div className="p-6 w-full max-w-6xl min-h-screen">
-        <img src="Logo.png" alt="" className="h-[8rem] m-auto" />
+        <img src="icon.jpg" alt="" className="h-[8rem] m-auto" />
         <form onSubmit={handleSubmit} className="bg-gray-100 pt-8 pb-7 px-5 w-full md:w-[75vw] mt-5 rounded-lg">
           {/* Customer Info */}
           <div className="flex flex-col gap-4">
